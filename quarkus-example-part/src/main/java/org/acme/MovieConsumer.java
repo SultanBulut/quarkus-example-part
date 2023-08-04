56620a3d096ce71cd8683f33e250cf607028e3de
sultan <sultan.bulut@std.yeditepe.edu.tr>
2023-08-04 12:35:50 +0300

    first

--- quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
+++ quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
@@ -3,6 +3,7 @@ package org.acme;
 import io.smallrye.reactive.messaging.kafka.Record;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
+
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
@@ -11,8 +12,26 @@ public class MovieConsumer {
     private final Logger logger = Logger.getLogger(MovieConsumer.class);
 
     @Incoming("movies-in")
-    public void receive(Record<Integer, String> record) {
-        logger.infof("Got a movie: %d - %s", record.key(), record.value());
+    public void receive(Movie movie) {
+        logger.infof("Got a movie: %d - %s (Directed by: %s)", movie.getReleaseYear(), movie.getTitle(), movie.getDirector() );
+    }
+}
+
+/*package org.acme;
+
+import io.smallrye.reactive.messaging.kafka.Record;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MovieConsumer {
+
+    private final Logger logger = Logger.getLogger(MovieConsumer.class);
+
+    @Incoming("movies-in")
+    public void receive( String record) {
+        logger.infof("Got a movie:  - %s", record);
     }
 }
 /*package org.acme;
--- quarkus-example-part/src/main/java/org/acme/MovieProducer.java
+++ quarkus-example-part/src/main/java/org/acme/MovieProducer.java
@@ -1,5 +1,25 @@
 package org.acme;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import jakarta.inject.Inject;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MovieProducer {
+
+    @Inject
+    @Channel("movies-out")
+    Emitter<Movie> emitter;
+
+    public void sendMovieToKafka(Movie movie) {
+        emitter.send(movie);
+    }
+}
+
+/*package org.acme;
+
 import io.smallrye.reactive.messaging.kafka.Record;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
