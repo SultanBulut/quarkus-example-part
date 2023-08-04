c9c40b1fbb7073769009dcef4a30cdbe478b62cd
fatih <fatihfurkanbilsel@gmail.com>
2023-08-04 13:21:20 +0300

    changes (swagger q/swagger)

--- quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
+++ quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
@@ -1,6 +1,10 @@
 package org.acme;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.smallrye.reactive.messaging.kafka.Record;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -8,11 +12,24 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class MovieConsumer {
 
+    @Inject
+    ObjectMapper objectMapper;
+    @Inject
+    MovieRepository movieRepository;
+
     private final Logger logger = Logger.getLogger(MovieConsumer.class);
 
     @Incoming("movies-in")
+    @Transactional
     public void receive(Record<Integer, String> record) {
         logger.infof("Got a movie: %d - %s", record.key(), record.value());
+        try {
+            MovieDTO movieDTO = objectMapper.readValue(record.value(), MovieDTO.class);
+            Movie movie = new Movie(movieDTO.getTitle(), movieDTO.getDirector(), movieDTO.getReleaseYear());
+            movieRepository.persist(movie);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 /*package org.acme;
