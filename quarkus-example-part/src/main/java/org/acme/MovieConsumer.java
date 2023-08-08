1d322804d0a3cf02a01f7299efcf2899d3ae59ae
fatih <fatihfurkanbilsel@gmail.com>
2023-08-08 15:30:26 +0300

    fix

--- quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
+++ quarkus-example-part/src/main/java/org/acme/MovieConsumer.java
@@ -1,6 +1,7 @@
 
 package org.acme;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import org.apache.camel.builder.RouteBuilder;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -16,65 +17,9 @@ public class MovieConsumer extends RouteBuilder {
                 .bean(this, "processMovie");
     }
 
+    @Transactional
     public void processMovie(MovieDTO movieDTO) {
         Movie movie = new Movie(movieDTO.getTitle(), movieDTO.getDirector(), movieDTO.getReleaseYear());
-
         movieRepository.persist(movie);
     }
 }
-
-
-/*package org.acme;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.smallrye.reactive.messaging.kafka.Record;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.jboss.logging.Logger;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MovieConsumer {
-
-    @Inject
-    ObjectMapper objectMapper;
-    @Inject
-    MovieRepository movieRepository;
-
-    private final Logger logger = Logger.getLogger(MovieConsumer.class);
-
-    @Incoming("movies-in")
-    @Transactional
-    public void receive(Record<Integer, String> record) {
-        logger.infof("Got a movie: %d - %s", record.key(), record.value());
-        try {
-            MovieDTO movieDTO = objectMapper.readValue(record.value(), MovieDTO.class);
-            Movie movie = new Movie(movieDTO.getTitle(), movieDTO.getDirector(), movieDTO.getReleaseYear());
-            movieRepository.persist(movie);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}*/
-
-/*package org.acme;
-
-import io.smallrye.reactive.messaging.kafka.Record;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.jboss.logging.Logger;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MovieConsumer {
-
-    private final Logger logger = Logger.getLogger(MovieConsumer.class);
-
-    @Incoming("movies-in")
-    public void receive(Movie movie) {
-        logger.infof("Got a movie: %d - %s (Directed by: %s)", movie.getReleaseYear(), movie.getTitle(), movie.getDirector() );
-    }
-}*/
-
--- quarkus-example-part/src/main/java/org/acme/MovieProducer.java
+++ quarkus-example-part/src/main/java/org/acme/MovieProducer.java
@@ -14,24 +14,3 @@ public class MovieProducer {
         producerTemplate.sendBody("kafka:movies-out", movieDTO);
     }
 }
-
-/*package org.acme;
-
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-
-import jakarta.inject.Inject;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MovieProducer {
-
-    @Inject
-    @Channel("movies-out")
-    Emitter<MovieDTO> emitter;
-
-    public void sendMovieToKafka(MovieDTO movieDTO) {
-        emitter.send(movieDTO);
-    }
-}*/
-
